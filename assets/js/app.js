f23787bf00e39775924f30636bceab0f8de71a7c
Suck To Be Me <dpnursanti@gmail.com>
2019-11-12 16:51:34 +0800

    button attack 80% done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 console.log("app is active");
+
+const ATTACK_VALUE = 10;
+let chooseMaxLife = 100;
+let currentMonsterHealth = chooseMaxLife;
+let currentPlayerHealth = chooseMaxLife;
+
+adjustHealthBars(chooseMaxLife);
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth = currentMonsterHealth - damage;
+}
+attackBtn.addEventListener("click", attackHandler);
