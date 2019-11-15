046d49746e3d2af1b9140af8f4daf5edace50bca
Suck To Be Me <dpnursanti@gmail.com>
2019-11-16 07:26:53 +0800

    button ultimate done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,40 @@
 console.log("app is active");
 
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK = 14;
+const STRONG_ATTACK = 17;
 let chooseMaxLife = 100;
 let currentMonsterHealth = chooseMaxLife;
 let currentPlayerHealth = chooseMaxLife;
 
 adjustHealthBars(chooseMaxLife);
+
+function attackMonster(type) {
+  let maxDamage;
+  if (type === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else {
+    maxDamage = STRONG_ATTACK;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("you win!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("you lose!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("draw!!!");
+  }
+}
+
 function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth = currentMonsterHealth - damage;
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
 }
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
